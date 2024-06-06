01ce8c87ea6cf49e3178c74d946307b758506a2e
Elliot <141949359+webElliot@users.noreply.github.com>
2024-06-06 14:26:14 +0100

    added support for multiple file extensions e.g CSV / txt ...

--- entities/names.py
+++ entities/names.py
@@ -6,12 +6,16 @@ def get_names(filename: str) -> list[str]:
     return usernames
 
 def save_outputs(filename: str, valid: list[str], banned: list[str]):
+    filename_suffix = filename.split('.')[1]
+
+    filename = filename.split('.')[0]
+
     if valid:
-        with open(os.path.join('data', 'output', f'{filename}_valid.txt'), 'w') as x:
+        with open(os.path.join('data', 'output', f'{filename}_valid.{filename_suffix}'), 'w') as x:
             for username in valid:
                 x.write(f'{username}\n')
 
     if banned:
-        with open(os.path.join('data', 'output', f'{filename}_banned.txt'), 'w') as x:
+        with open(os.path.join('data', 'output', f'{filename}_banned.{filename_suffix}'), 'w') as x:
             for username in banned:
                 x.write(f'{username}\n')
